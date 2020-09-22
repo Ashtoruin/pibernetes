bb48141453e0cdb7bc77a0585dc67ad00c64ffbf
RJ <ashtoruin@gmail.com>
2020-09-22 19:50:16 +0100

    clients fo dayz

--- roles/pxeboot/scripts/test.sh
+++ roles/pxeboot/scripts/test.sh
@@ -4,9 +4,12 @@ image="/nfs/images/2020-08-20-raspios-buster-arm64-lite.img"
 start_bytes=($(sudo sfdisk --json $image | jq -r '.partitiontable.partitions | .[].start'))
 
 mount $image -o loop,offset=$(( 512 * ${start_bytes[0]})) /imgtmp
-rsync -a /imgtmp/ /nfs/client/boot/
+rsync -a /imgtmp/ /nfs/images/boot
 umount /imgtmp
 mount $image -o loop,offset=$(( 512 * ${start_bytes[1]})) /imgtmp
-rsync -a /imgtmp/ /nfs/client/root/
+cat << EOF | chroot imgtmp/
+systemctl enable ssh
+EOF
+rsync -a /imgtmp/ /nfs/images/root/
 umount /imgtmp
 exit 0
