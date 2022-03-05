43b0157f652b6088ed2f6088368fdd538c918cf7
Roland Bernau <roland@bernau.dev>
2022-03-05 12:43:06 +0000

    Adding github actions

---
+++ wrapper_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+ 
+# Install required pi-gen dependencies
+sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip \
+dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl bc
+ 
+# Get pi-gen git repo
+git clone https://github.com/RPi-Distro/pi-gen.git
+pushd pi-gen
+chmod +x build.sh
+popd
+
+export IMG_NAME=edge-raspbian
+export RELEASE=buster
+export DEPLOY_ZIP=1
+export LOCALE_DEFAULT=en_US.UTF-8
+export TARGET_HOSTNAME=edge-dev-pi
+export KEYBOARD_KEYMAP=us
+export KEYBOARD_LAYOUT="English (US)"
+export TIMEZONE_DEFAULT=America/New_York
+export FIRST_USER_NAME=iot
+export FIRST_USER_PASS="${pi_pwd}"
+export ENABLE_SSH=1
+
+./pi-gen/build.sh
+
