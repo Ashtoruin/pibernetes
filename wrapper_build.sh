7eaa273076cc87647f63a899224f0383a30239ed
Roland Bernau <roland@bernau.dev>
2022-03-05 12:53:44 +0000

    adding workflow

--- wrapper_build.sh
+++ wrapper_build.sh
@@ -3,12 +3,6 @@
 # Install required pi-gen dependencies
 sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip \
 dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl bc
- 
-# Get pi-gen git repo
-git clone https://github.com/RPi-Distro/pi-gen.git
-pushd pi-gen
-chmod +x build.sh
-popd
 
 export IMG_NAME=edge-raspbian
 export RELEASE=buster
