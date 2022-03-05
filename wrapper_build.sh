d1dd4c1a94b85509ac470b87b909afaf3cbaf63d
Roland Bernau <roland@bernau.dev>
2022-03-05 13:03:15 +0000

    wip

--- wrapper_build.sh
+++ wrapper_build.sh
@@ -4,5 +4,6 @@
 sudo apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree zip \
 dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl bc
 
-sudo ./pi-gen/build.sh
-
+cp config pi-gen/config
+cd pi-gen
+sudo ./build.sh
